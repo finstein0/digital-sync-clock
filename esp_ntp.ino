ed7ca4df8f5c043179ab73c8c82b3c25e3df4e2f
finstein0 <42037895+finstein0@users.noreply.github.com>
2021-05-01 17:28:40 +0530

    Update esp_ntp.ino

--- esp_ntp.ino
+++ esp_ntp.ino
@@ -45,59 +45,8 @@ void setup() {
 
 void loop() {
   timeClient.update();
-
-  /*unsigned long epochTime = timeClient.getEpochTime();
-  Serial.print(epochTime);
-  Serial.println("U");
-  */
-  
   String formattedTime   = timeClient.getFormattedTime();
-  
-  //Serial.print("Formatted Time: ");
   Serial.print(formattedTime);
   Serial.print ("U");
-  //Serial1.write(cString);  
-  
-  /**int currentHour = timeClient.getHours();
-  Serial.print("Hour: ");
-  Serial.println(currentHour);  
-
-  int currentMinute = timeClient.getMinutes();
-  Serial.print("Minutes: ");
-  Serial.println(currentMinute); 
-   
-  int currentSecond = timeClient.getSeconds();
-  Serial.print("Seconds: ");
-  Serial.println(currentSecond);  
-
-  String weekDay = weekDays[timeClient.getDay()];
-  Serial.print("Week Day: ");
-  Serial.println(weekDay);    
-
-  //Get a time structure
-  struct tm *ptm = gmtime ((time_t *)&epochTime); 
-
-  int monthDay = ptm->tm_mday;
-  Serial.print("Month day: ");
-  Serial.println(monthDay);
-
-  int currentMonth = ptm->tm_mon+1;
-  Serial.print("Month: ");
-  Serial.println(currentMonth);
-
-  String currentMonthName = months[currentMonth-1];
-  Serial.print("Month name: ");
-  Serial.println(currentMonthName);
-
-  int currentYear = ptm->tm_year+1900;
-  Serial.print("Year: ");
-  Serial.println(currentYear);
-
-  //Print complete date:
-  String currentDate = String(currentYear) + "-" + String(currentMonth) + "-" + String(monthDay);
-  Serial.print("Current date: ");
-  Serial.println(currentDate);*/
-
-
   delay(1000);
 }
